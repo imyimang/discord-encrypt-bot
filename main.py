07c5be7e879ec7df6f31e72b22a06b674aa89382
yimang <lucasbbtung@gmail.com>
2024-05-24 14:33:02 +0800

    fix bugs

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ async def help(ctx):
 async def generate_key(ctx):
     try:
         key = Fernet.generate_key()
+        if not os.path.exists("keys"):os.makedirs("keys")
         with open(f"keys/{ctx.user.id}.key", "wb") as key_file:
             key_file.write(key)
             key = key.decode('utf-8')
@@ -53,11 +54,12 @@ async def generate_key(ctx):
 @bot.tree.command(name="查詢金鑰", description="查詢你當前的加密金鑰") 
 async def check_key(ctx):
 
+    if not os.path.exists("keys"):os.makedirs("keys")
     path = f"keys/{ctx.user.id}.key"
     if not os.path.exists(path) or os.path.getsize(path)==0:
         await ctx.response.send_message(f"並無已生成的金鑰，請使用/加密 或 /生成金鑰來獲取金鑰", ephemeral=True)
         return
-    
+        
     try:
         with open(path, "rb") as file:
             data = file.read()
@@ -71,6 +73,7 @@ async def check_key(ctx):
 @bot.tree.command(name="設定金鑰", description="重新設定你的加密金鑰(先前加密的檔案將無法解密)") 
 async def set_key(ctx, key:str):
     try:
+        if not os.path.exists("keys"):os.makedirs("keys")
         with open(f"keys/{ctx.user.id}.key", "wb") as key_file:
             msg_2 = bytes(key, 'utf-8')
             key_file.write(msg_2)
@@ -87,6 +90,7 @@ async def encrypt_command(ctx: discord.Interaction, the_file: discord.Attachment
         await ctx.response.send_message("檔案過大!檔案不可大於15MB", ephemeral=True)
         return
     
+    if not os.path.exists("keys"):os.makedirs("keys")
     path = f"keys/{ctx.user.id}.key"
 
     if (not os.path.exists(path) or os.path.getsize(path)==0) and not optional_key:  
@@ -95,13 +99,11 @@ async def encrypt_command(ctx: discord.Interaction, the_file: discord.Attachment
             key_file.write(key)
             key = key.decode('utf-8')
         no_key = f"未找到已經存在的金鑰，已生成新的金鑰:\n**{key}**\n請妥善保管，遺失後將無法解密檔案\n\n"     
-    else:
-        no_key = ""
+    else:no_key = ""
 
     try:
-        if ctx.user == bot.user:
-            return
-
+        if ctx.user == bot.user:return
+        if not os.path.exists("temporary"):os.makedirs("temporary")
         await ctx.response.defer(ephemeral=True)
         await download_file(the_file.url, the_file.filename)
         print(f'{the_file.filename} 已下載')
@@ -109,13 +111,13 @@ async def encrypt_command(ctx: discord.Interaction, the_file: discord.Attachment
             encrypt_file(the_file.filename, ctx.user.id,optional_key)
         else:
             encrypt_file(the_file.filename, ctx.user.id,None)
-        file = discord.File(f"{str(the_file.filename)}.enc")
+        file = discord.File(f"temporary/{(the_file.filename)}.enc")
         await ctx.followup.send(content = f"{no_key}加密成功",file = file, ephemeral=True)
 
     except Exception as e:
         await ctx.followup.send(f"加密失敗:\n**{e}**")
-    os.remove(the_file.filename)
-    os.remove(f"{the_file.filename}.enc")
+    os.remove(f'temporary/{the_file.filename}')
+    os.remove(f'temporary/{the_file.filename}.enc')
 
 
 
@@ -126,34 +128,34 @@ async def decrypt_command(ctx: discord.Interaction,the_file: discord.Attachment,
         await ctx.response.send_message("檔案過大!檔案不可大於15MB", ephemeral=True)
         return
     
+    if not os.path.exists("keys"):os.makedirs("keys")
     path = f"keys/{ctx.user.id}.key"
     if (not os.path.exists(path) or os.path.getsize(path)==0) and not optional_key:
         await ctx.response.send_message("找不到解密金鑰", ephemeral=True)
         return
     
     try:
-        if ctx.user == bot.user:
-            return
+        if ctx.user == bot.user:return
         # 下載並儲存附件
         if not the_file.filename.endswith(".enc"):
             await ctx.response.send_message("解密失敗:\n**請提供正確的加密檔案(.enc結尾)**", ephemeral=True)
             return
         await ctx.response.defer(ephemeral=True)
+        if not os.path.exists("temporary"):os.makedirs("temporary")
         await download_file(the_file.url, the_file.filename)
         print(f'{the_file.filename} 已下載')
         if optional_key:
             decrypt_file(the_file.filename, ctx.user.id,optional_key)
         else:
             decrypt_file(the_file.filename, ctx.user.id,None)
-        file_name = str(the_file.filename)
-        file_name = file_name[:-4]
-        file = discord.File(file_name)
+        
+        file = discord.File(f"temporary/{the_file.filename[:-4]}")
         await ctx.followup.send(content = "解密成功",file = file, ephemeral=True)
 
     except Exception as e:
         await ctx.followup.send(f"解密失敗:\n**{e}**")
-    os.remove(the_file.filename)
-    os.remove(file_name)
+    os.remove(f'temporary/{the_file.filename}')
+    os.remove(f'temporary/{the_file.filename[:-4]}')
 
 
 
@@ -172,14 +174,12 @@ def encrypt_file(filename,userid,optional_key):
         key = load_key(userid)
     f = Fernet(key)
     
-    with open(filename, "rb") as file:
+    with open(f"temporary/{filename}", "rb") as file:
         file_data = file.read()
 
-
-        
     encrypted_data = f.encrypt(file_data)
     
-    with open(filename + ".enc", "wb") as file:
+    with open(f"temporary/{filename}" + ".enc", "wb") as file:
         file.write(encrypted_data)
 
 
@@ -193,15 +193,13 @@ def decrypt_file(filename,userid,optional_key):
         key = load_key(userid)
     f = Fernet(key)
     
-    with open(filename, "rb") as file:
+    with open(f"temporary/{filename}", "rb") as file:
         encrypted_data = file.read()
 
-        
     decrypted_data = f.decrypt(encrypted_data)
-    
-    with open(filename[:-4], "wb") as file:
+    filename = f"temporary/{filename}"[:-4]
+    with open(filename, "wb") as file:
         file.write(decrypted_data)
-
 
 
 
@@ -210,7 +208,7 @@ async def download_file(url, filename):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             if response.status == 200:
-                with open(filename, 'wb') as f:
+                with open(f"temporary/{filename}", 'wb') as f:
                     f.write(await response.read())
 
 
